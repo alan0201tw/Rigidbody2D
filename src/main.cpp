531e2c6d8248ce8ea69c9273a2b0d958c80f3fa3
alan0201tw <alan0201tw@gmail.com>
2019-10-15 14:49:09 +0800

    support reshape window

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 
 namespace
 {
-    const float deltaTime = 1.0 / 60.0f;
+    const float deltaTime = 1.0f / 60.0f;
     const uint32_t positional_correction_iterations = 10;
     const float accumulate_upper_bound = 
         std::max(deltaTime, 0.1f);
@@ -23,6 +23,10 @@ namespace
     Scene scene(deltaTime, positional_correction_iterations,
         std::make_shared<ExplicitEulerIntegrator>()
     );
+
+    float screen_width = 600;
+    float screen_height = 600;
+
     typedef linalg::aliases::float2 float2;
 }
 
@@ -82,12 +86,29 @@ public:
 
     }
 
+    static void Reshape(int width, int height)
+    {
+        screen_width = width;
+        screen_height = height;
+
+        glViewport( 0, 0, width, height );
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        // when init, window size is 600x600 mapping to [-30 ~ 30]
+        float2 ortho_size((float)width / 20.0f, (float)height / 20.0f);
+        
+        // gluOrtho2D sets up a two-dimensional orthographic viewing region.
+        // This is equivalent to calling glOrtho with near = -1 and far = 1 .
+        gluOrtho2D(-ortho_size.x, ortho_size.x, -ortho_size.y, ortho_size.y);
+    }
+
     static void Mouse(int button, int state, int x, int y)
     {
         if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
         {
-            // std::cout << "x , y = " << x << " , " << y << std::endl;
-            float2 position = float2( (float)x / 10.0f - 30.0f, (float)y / -10.0f + 30.0f );
+            float2 ortho_size((float)screen_width / 20.0f, (float)screen_height / 20.0f);
+            float2 position = float2( (float)x / 10.0f - ortho_size.x, (float)y / -10.0f + ortho_size.y );
+
             std::shared_ptr<Circle> shape = std::make_shared<Circle>(
                 3.0f
             );
@@ -95,7 +116,9 @@ public:
         }
         if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
         {
-            float2 position = float2( (float)x / 10.0f - 30.0f, (float)y / -10.0f + 30.0f );
+            float2 ortho_size((float)screen_width / 20.0f, (float)screen_height / 20.0f);
+            float2 position = float2( (float)x / 10.0f - ortho_size.x, (float)y / -10.0f + ortho_size.y );
+
             std::shared_ptr<AABB> shape = std::make_shared<AABB>(
                 //float2 (3, 3)
                 float2( drand48() * 5 + 3, drand48() * 5 + 3 )
@@ -111,22 +134,16 @@ int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(600, 600);
+    glutInitWindowSize(screen_width, screen_height);
     glutCreateWindow("PhyEngine");
     glutDisplayFunc(GLUTCallback::MainLoop);
     glutKeyboardFunc(GLUTCallback::Keyboard);
     glutMouseFunc(GLUTCallback::Mouse);
+    glutReshapeFunc(GLUTCallback::Reshape);
 
     // NOTE : please do not use glutTimerFunc.
     // We need you to practice on designing the loop itself,
     // resolving the different update rate of physics and rendering.
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    // gluOrtho2D sets up a two-dimensional orthographic viewing region.
-    // This is equivalent to calling glOrtho with near = -1 and far = 1 .
-    gluOrtho2D(-30, 30, -30, 30);
-    //glOrtho(-300, 300, -300, 300, 0.01, 1000.0);
 
     // fill in the scene
     // floor
