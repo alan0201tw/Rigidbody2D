783fda9f41797bb3efe8cceaae14507d863cf809
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2019-11-15 01:23:49 +0800

    add namespace to look nicer in util

--- include/util.hpp
+++ include/util.hpp
@@ -6,12 +6,12 @@ using linalg::aliases::float2;
 
 float2 safe_normalize(float2 a)
 {
-    float length_a = length(a);
+    float length_a = linalg::length(a);
     return (length_a == 0.0f) ? float2(0.0f, 0.0f) : a / length_a;
 }
 
 float safe_distance(float2 a, float2 b)
 {
-    float length_ab = length(a - b);
+    float length_ab = linalg::length(a - b);
     return (length_ab == 0.0f) ? 0.0f : length_ab;
 }
